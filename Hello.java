73a3ff739a2dbf494c1c4d56b5f351a944c8d695
Dhruv Chaurasia <dhruvchaurasia52@gmail.com>
2023-03-24 02:16:45 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,32 @@
+// class Hello {
+// public static void main(String a[]) {
+
+// System.out.println("Hello World, Java");
+// System.out.println(7+6);
+// }
+// }
+
+// class Hello {
+// public static void main(String a[]) {
+
+// System.out.println(44 + 6);
+// }
+// }
+
+// class Hello {
+// public static void main(String a[]) {
+// int num1 = 3;
+// int num2 = 9;
+// int Result = num1 + num2;
+
+// System.out.println(Result);
+// }
+// }
+
+// class Hello {
+// public static void main(String a[]) {
+// int num1 = 9;
+// byte by = 54;
+// // System.out.println(result);
+// }
+// }
